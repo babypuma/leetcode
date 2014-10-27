5f53594e1c01311678c423ba177c744d8541169d
Jeremy Zhao <jqzhao@live.com>
2014-10-27 20:04:29 +0800

    solution to Construct Binary Tree from Preorder and Inorder Traversal

---
+++ construct_binary_tree_from_preorder_and_inorder_traversal.h
@@ -0,0 +1,64 @@
+#include <vector>
+using std::vector;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+	public:
+		TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
+			if (preorder.empty() || inorder.empty()) {
+				return NULL;
+			}
+			return build(preorder, 0, preorder.size()-1, inorder, 0, inorder.size()-1);
+		}
+
+		TreeNode *build(vector<int> &preorder, int i, int j, vector<int> &inorder, int m, int n) {
+			if (i == j) {
+				return new TreeNode(preorder[i]);
+			}
+
+			TreeNode *node = new TreeNode(preorder[i]);
+			int root = find(inorder, preorder[i], m, n);
+			if (root == m) {
+				node->right = build(preorder, i+1, j, inorder, m+1, n);
+			}
+			else if (root == n) {
+				node->left = build(preorder, i+1, j, inorder, m, n-1);
+			}
+			else {
+				node->left = build(preorder, i+1, (i+1)+(root-m-1), inorder, m, root-1);
+				node->right = build(preorder, (i+1)+(root-m-1)+1, j, inorder, root+1, n);
+			}
+
+			return node;
+		}
+
+		int find(vector<int> &order, int val, int i, int j) {
+			for (; i < j; i++) {
+				if (order[i] == val) {
+					return i;
+				}
+			}
+			return j;
+		}
+
+		bool isSameTree(TreeNode *p, TreeNode *q) {
+			if (p == NULL && q == NULL) {
+				return true;
+			}
+			if ((p == NULL && q != NULL) || (p != NULL && q == NULL)) {
+				return false;
+			}
+
+			if (p->val == q->val) {
+				return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+			} 
+			
+			return false;
+		}
+};
---
+++ construct_binary_tree_from_preorder_and_inorder_traversal_test.cpp
@@ -0,0 +1,37 @@
+#include <gtest/gtest.h>
+#include "construct_binary_tree_from_preorder_and_inorder_traversal.h"
+
+class SolutionTest : public ::testing::Test {
+    protected:
+        virtual void SetUp() {
+        }
+        virtual void TearDown() {
+        }
+
+        Solution solution_;
+};
+
+TEST_F(SolutionTest, case1) {
+	TreeNode *root = new TreeNode(1);
+	root->left = new TreeNode(2);
+	root->right = new TreeNode(3);
+	root->left->left = new TreeNode(4);
+	root->left->right = new TreeNode(5);
+	root->right->left = new TreeNode(6);
+	root->right->right = new TreeNode(7);
+
+	int pre[] = {1, 2, 4, 5, 3, 6, 7};
+	int in[] = {4, 2, 5, 1, 6, 3, 7};
+	vector<int> preorder(pre, pre + sizeof(pre)/sizeof(int));
+	vector<int> inorder(in, in + sizeof(in)/sizeof(int));
+	TreeNode *node = solution_.buildTree(preorder, inorder);
+	EXPECT_TRUE(solution_.isSameTree(root, node));
+}
+
+TEST_F(SolutionTest, case2) {
+	vector<int> preorder;
+	vector<int> inorder;
+	TreeNode *node = solution_.buildTree(preorder, inorder);
+	EXPECT_TRUE(node == NULL);
+}
+
