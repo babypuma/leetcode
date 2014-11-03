7f0b8b7bb9e85500143349de468b5e349f9cb4f7
Jeremy Zhao <jqzhao@live.com>
2014-11-04 05:47:11 +0800

    solution to Reverse words in a string

---
+++ reverse_words_in_a_string.h
@@ -0,0 +1,55 @@
+#include <string.h>
+#include <string>
+using std::string;
+
+class Solution {
+	public:
+	    void reverseWords(string &s) {
+			int length = s.size();
+			if (length == 0) {
+				return;
+			}
+			string reversed_string;
+			char str[length + 1];
+			strcpy(str, s.c_str());
+			reverse(str, 0, length - 1);
+
+			int begin = 0;
+			int end = 0;
+			int i;
+			for (i = 0; i < length; i++) {
+				if (str[i] != ' ') {
+					end = i;
+				}
+				else {
+					if (begin != i) {
+						reverse(str, begin, end);
+						reversed_string += string(str + begin, end - begin + 1);
+						reversed_string += " ";
+					} 
+					begin = i + 1;
+				}
+			}
+
+			if (begin <= end) {
+				reverse(str, begin, end); 
+				reversed_string += string(str + begin, end - begin + 1);
+			}
+			strcpy(str, reversed_string.c_str());
+			length = reversed_string.size();
+			for (i = length - 1; i >= 0 && str[i] == ' '; i--) {
+			}
+			end = i;
+			s = string(str, end + 1);
+		}
+
+		void reverse(char str[], int begin, int end) {
+			int middle = (end - begin) / 2;
+			char ch;
+			for (int i = 0; i <= middle; i++) {
+				ch = str[begin + i];
+				str[begin + i] = str[end - i];
+				str[end - i] = ch;
+			}
+		}
+};
---
+++ reverse_words_in_a_string_test.cpp
@@ -0,0 +1,49 @@
+#include <gtest/gtest.h>
+#include "reverse_words_in_a_string.h"
+
+class SolutionTest : public ::testing::Test {
+    protected:
+        virtual void SetUp() {
+        }
+        virtual void TearDown() {
+        }
+
+        Solution solution_;
+};
+
+TEST_F(SolutionTest, case1) {
+	string str("the sky is blue");
+	solution_.reverseWords(str);
+	EXPECT_STREQ(str.c_str(), "blue is sky the");
+}
+
+TEST_F(SolutionTest, case2) {
+	string str(" ");
+	solution_.reverseWords(str);
+	EXPECT_STREQ(str.c_str(), "");
+}
+
+TEST_F(SolutionTest, case3) {
+	string str("");
+	solution_.reverseWords(str);
+	EXPECT_STREQ(str.c_str(), "");
+}
+
+TEST_F(SolutionTest, case4) {
+	string str(" 1");
+	solution_.reverseWords(str);
+	EXPECT_STREQ(str.c_str(), "1");
+}
+
+TEST_F(SolutionTest, case5) {
+	string str("  a  b");
+	solution_.reverseWords(str);
+	EXPECT_STREQ(str.c_str(), "b a");
+}
+
+TEST_F(SolutionTest, case6) {
+	string str("a");
+	solution_.reverseWords(str);
+	EXPECT_STREQ(str.c_str(), "a");
+}
+
