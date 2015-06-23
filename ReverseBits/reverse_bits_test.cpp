a8eb98448c10aadc516d11030adc037441407a76
Jeremy Zhao <jqzhao@live.com>
2015-06-24 00:14:08 +0800

    solution to Reverse Bits

---
+++ ReverseBits/reverse_bits_test.cpp
@@ -0,0 +1,26 @@
+/*
+ * Author : Jeremy Zhao
+ * Email  : jqzhao@live.com
+ * Date   : 2015/06/16
+ *
+ * Source : https://leetcode.com/problems/reverse-bits/
+ * Problem:	Reverse Bits
+ *
+ */
+#include <gtest/gtest.h>
+#include "reverse_bits.h"
+
+class SolutionTest : public ::testing::Test {
+    protected:
+        virtual void SetUp() {
+        }
+        virtual void TearDown() {
+        }
+
+        Solution solution_;
+};
+
+TEST_F(SolutionTest, case1) {
+	EXPECT_EQ(solution_.reverseBits(43261596), 964176192);
+}
+
