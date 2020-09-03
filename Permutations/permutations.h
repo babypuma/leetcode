8a8afdb21a165407a0755dd5e5042fd31909208c
Jeremy Zhao <jqzhao@live.com>
2020-09-03 09:52:26 +0800

    updates Permutations

--- Permutations/permutations.h
+++ Permutations/permutations.h
@@ -1,88 +1,43 @@
 /*
  * Author : Jeremy Zhao
  * Email  : jqzhao@live.com
- * Date   : 2014/11/06
+ * Date   : 2020/09/03
  *
- * Source : https://oj.leetcode.com/problems/permutations/
+ * Source : https://leetcode.com/problems/permutations/
  * Problem:	Permutations
- * Description: 
- *	Given a collection of numbers, return all possible permutations.  
- *	For example,
- *	[1,2,3] have the following permutations:
- *	[1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
- *
  */
 #include <vector>
+#include <unordered_set>
 using std::vector;
+using std::unordered_set;
 
 class Solution {
-	public:
-		vector<vector<int> > permute(vector<int> &num) {
-			vector<vector<int> > vec;
-			vec.push_back(num);
+ public:
+   vector<vector<int> > permute(vector<int>& nums) {
+     std::sort(nums.begin(), nums.end());
 
-			size_t length = factorial(num.size());
-			while (vec.size() < length) {
-				nextPermutation(num);
-				vec.push_back(num);
-			}
+     vector<int> solution;
+     unordered_set<int> index;
+     vector<vector<int> > solutions;
+     backtrack(nums, solution, index, solutions);
+     return solutions;
+   }
 
-			return vec;
-		}
-
-	    void nextPermutation(vector<int> &num) {
-			int length = num.size();
-			int i = length - 2;
-			for (; i >= 0; i--) {
-				if (num[i] < num[i + 1]) {
-					break;
-				}
-			}
-			if (i < 0) {
-				reverse(num, 0, length - 1);
-				return;
-			}
-
-			int index = length - 1;
-			for (; index >= 0; index--) {
-				if (num[index] > num[i]) {
-					break;
-				}
-			}
-			swap(num, i, index);
-			reverse(num, i + 1, length - 1);
-		}
-
-		void reverse(vector<int> &vec, int begin, int end) {
-			int length = vec.size();
-			if (end > length - 1 || end - begin < 1) {
-				return;
-			}
-			int mid = (end - begin)/2; 
-			int temp;
-			for (int i = 0; i <= mid; i++) {
-				temp = vec[begin + i];
-				vec[begin + i] = vec[end - i];
-				vec[end - i] = temp;
-			}
-		}
-
-		void swap(vector<int> &vec, size_t i, size_t j) {
-			size_t length = vec.size();
-			if (i == j || i >= length || j >= length) {
-				return;
-			}
-			int temp = vec[i];
-			vec[i] = vec[j];
-			vec[j] = temp;
-		}
-
-		size_t factorial(size_t n) {
-			size_t sum = 1;
-			for (size_t i = 2; i < n + 1; i++) {
-				sum *= i;
-			}
-
-			return sum;
-		}
+ private:
+   void backtrack(const vector<int>& candidates, vector<int>& solution, unordered_set<int>& index, vector<vector<int> >& solutions) {
+     if (solution.size() == candidates.size()) {
+       solutions.push_back(solution);
+       return;
+     }
+     for (size_t i = 0; i < candidates.size(); ++i) {
+       if (index.find(i) != index.end()) {
+         continue;
+       }
+       solution.push_back(candidates[i]);
+       index.insert(i);
+       backtrack(candidates, solution, index, solutions);
+       index.erase(i);
+       solution.pop_back();
+     }
+   }
 };
--- Permutations/permutations_test.cpp
+++ Permutations/permutations_test.cpp
@@ -1,12 +1,3 @@
-/*
- * Author : Jeremy Zhao
- * Email  : jqzhao@live.com
- * Date   : 2014/11/06
- *
- * Source : https://oj.leetcode.com/problems/permutations/
- * Problem:	Permutations
- *
- */
 #include <gtest/gtest.h>
 #include "permutations.h"
 
@@ -20,16 +11,12 @@ class SolutionTest : public ::testing::Test {
         Solution solution_;
 };
 
-TEST_F(SolutionTest, case1) {
-	int a[] = {1, 2, 3};
-	vector<int> vec(a, a + sizeof(a)/sizeof(int));
-
-	vector<vector<int> > vec2d = solution_.permute(vec);
-	int b[][3] = {{1, 2, 3}, {1, 3, 2}, {2, 1, 3}, {2, 3, 1}, {3, 1, 2}, {3, 2, 1}};
-	for (size_t i = 0; i < sizeof(b)/sizeof(int[3]); i++) {
-		for (size_t j = 0; j < sizeof(b[i])/sizeof(int); j++) {
-			EXPECT_EQ(vec2d[i][j], b[i][j]);
-		}
-	}
+TEST_F(SolutionTest, permuteUnique) {
+  int array[] = {1,2,3};
+  vector<int> cand;
+  for (size_t i = 0; i < sizeof(array)/sizeof(array[0]); ++i) {
+    cand.push_back(array[i]);
+  }
+  vector<vector<int> > vec_2_dim = solution_.permute(cand);
+  EXPECT_EQ(vec_2_dim.size(), 6);
 }
-
