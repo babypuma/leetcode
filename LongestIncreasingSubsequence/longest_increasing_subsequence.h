e626e2e660ded409ae6a61bbc375ef0c9a1c277d
Jeremy Zhao <jqzhao@live.com>
2020-09-25 13:50:21 +0800

    Adds another solution for LIS

--- LongestIncreasingSubsequence/longest_increasing_subsequence.h
+++ LongestIncreasingSubsequence/longest_increasing_subsequence.h
@@ -15,6 +15,12 @@ using std::vector;
 class Solution {
  public:
    int lengthOfLIS(vector<int>& nums) {
+     return greedy(nums);
+   }
+
+ private:
+   // DP算法，dp[i]表示以num[i]为结尾的最长LIS，时间复杂度O(n^2)
+   int dp(vector<int>& nums) {
      vector<int> dp(nums.size(), 1);
      for (size_t i = 1; i < nums.size(); ++i) {
        for (size_t j = 0; j < i; ++j) {
@@ -30,5 +36,16 @@ class Solution {
        }
      }
      return max;
+   }
+
+   // Greedy算法，时间复杂度为O(nlogn)
+   int greedy(vector<int>& nums) {
+     vector<int> piles(nums.size(), INT_MAX);
+     for (size_t i = 0; i < nums.size(); ++i) {
+       vector<int>::iterator it = lower_bound(piles.begin(), piles.end(), nums[i]);
+       int j = it - piles.begin();
+       piles[j] = nums[i];
+     }
+     return lower_bound(piles.begin(), piles.end(), INT_MAX) - piles.begin();
    }
 };
