e0094fa317fd471a80f618fe4f1e11900ea4993f
Jeremy Zhao <jqzhao@live.com>
2020-09-25 12:52:17 +0800

    updates LongestIncreasingSubsequence

--- LongestIncreasingSubsequence/longest_increasing_subsequence.h
+++ LongestIncreasingSubsequence/longest_increasing_subsequence.h
@@ -2,6 +2,7 @@
  * Author : Jeremy Zhao
  * Email  : jqzhao@live.com
  * Date   : 2020/09/10
+ * Update : 2020/09/25
  *
  * Source : https://leetcode.com/problems/longest-increasing-subsequence/
  * Problem:	Longest Increasing Subsequence
@@ -14,17 +15,20 @@ using std::vector;
 class Solution {
  public:
    int lengthOfLIS(vector<int>& nums) {
-     int sz = nums.size();
-     vector<int32_t> piles(sz + 1, INT32_MAX);
-     for (size_t i = 0; i < sz; ++i) {
-       size_t j = std::lower_bound(piles.begin(), piles.end(), nums[i]) - piles.begin();
-       piles[j] = nums[i];
-     }
-     for (size_t i = 0; i < sz + 1; ++i) {
-       if (piles[i] == INT32_MAX) {
-         return i;
+     vector<int> dp(nums.size(), 1);
+     for (size_t i = 1; i < nums.size(); ++i) {
+       for (size_t j = 0; j < i; ++j) {
+         if (nums[j] < nums[i]) {
+           dp[i] = std::max(dp[i], dp[j] + 1);
+         }
        }
      }
-     return 0;
+     int max = 0;
+     for (size_t i = 0; i < dp.size(); ++i) {
+       if (dp[i] > max) {
+         max = dp[i];
+       }
+     }
+     return max;
    }
 };
