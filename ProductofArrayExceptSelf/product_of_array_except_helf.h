beec009e1417f34c52fce3b0a72399777bc43a71
Jeremy Zhao <jqzhao@live.com>
2020-11-06 02:14:59 +0800

    adds ProductOfArrayExceptSelf

--- ProductofArrayExceptSelf/product_of_array_except_helf.h
+++ ProductofArrayExceptSelf/product_of_array_except_helf.h
@@ -1,42 +1,30 @@
 /*
  * Author : Jeremy Zhao
  * Email  : jqzhao@live.com
- * Date   : 2015/06/16
+ * Date   : 2020/11/06
  *
  * Source : https://leetcode.com/problems/product-of-array-except-self/
  * Problem:	Product of Array Except Self
- * Description: 
- *	Given an array of n integers where n > 1, nums, return an array output such that 
- *	output[i] is equal to the product of all the elements of nums except nums[i].
- *
- *	Solve it without division and in O(n).
- *
- * 	For example, given [1,2,3,4], return [24,12,8,6].
- *
- * Follow up:
- *	Could you solve it with constant space complexity? (Note: The output array does not count 
- *	as extra space for the purpose of space complexity analysis.)
  *
  */
 #include <vector>
 using std::vector;
 
 class Solution {
-	public:
-	    vector<int> productExceptSelf(vector<int>& nums) {
-			int n = nums.size();
-			vector<int> vec(n, 1);
-			for (int i = 1; i < n; i++) {
-				vec[i] = nums[i - 1] * vec[i - 1];
-			}
-
-			int mul = nums[n - 1];
-			for (int i = n - 2; i > 0; i--) {
-				vec[i] *= mul;
-				mul *= nums[i];
-			}
-			vec[0] *= mul;
-			
-			return vec;
-		}
+ public:
+   vector<int> productExceptSelf(vector<int>& nums) {
+     int n = nums.size();
+     vector<int> prefix_product(n, 1);
+     vector<int> suffix_product(n, 1);
+     for (int i = 1; i < n; ++i) {
+       prefix_product[i] = prefix_product[i - 1] * nums[i - 1];
+     }
+     for (int i = n - 2; i >= 0; --i) {
+       suffix_product[i] = suffix_product[i + 1] * nums[i + 1];
+     }
+     for (int i = 0; i < n; ++i) {
+       prefix_product[i] *= suffix_product[i];
+     }
+     return prefix_product;
+   }
 };
--- ProductofArrayExceptSelf/product_of_array_except_helf_test.cpp
+++ ProductofArrayExceptSelf/product_of_array_except_helf_test.cpp
@@ -1,7 +1,7 @@
 /*
  * Author : Jeremy Zhao
  * Email  : jqzhao@live.com
- * Date   : 2015/06/16
+ * Date   : 2020/11/06
  *
  * Source : https://leetcode.com/problems/product-of-array-except-self/
  * Problem:	Product of Array Except Self
@@ -11,22 +11,22 @@
 #include "product_of_array_except_helf.h"
 
 class SolutionTest : public ::testing::Test {
-    protected:
-        virtual void SetUp() {
-        }
-        virtual void TearDown() {
-        }
+ protected:
+   virtual void SetUp() {
+   }
+   virtual void TearDown() {
+   }
 
-        Solution solution_;
+   Solution solution_;
 };
 
 TEST_F(SolutionTest, case1) {
-	int a[] = {1, 2, 3, 4};
-	int b[] = {24, 12, 8, 6};
-	vector<int> vec(a, a + sizeof(a) / sizeof(int));
-	vector<int> v = solution_.productExceptSelf(vec);
-	for (size_t i = 0; i < vec.size(); i++) {
-		EXPECT_EQ(v[i], b[i]);
-	}
+  int a[] = {1, 2, 3, 4};
+  int b[] = {24, 12, 8, 6};
+  vector<int> vec(a, a + sizeof(a) / sizeof(int));
+  vector<int> v = solution_.productExceptSelf(vec);
+  for (size_t i = 0; i < vec.size(); i++) {
+    EXPECT_EQ(v[i], b[i]);
+  }
 }
 
