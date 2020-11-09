4cbdb5976a65ec25abb83b03b4901370a1eab40a
Jeremy Zhao <jqzhao@live.com>
2020-11-09 15:24:16 +0800

    updates MaximumSubarray

--- MaximumSubarray/maximum_subarray.h
+++ MaximumSubarray/maximum_subarray.h
@@ -2,7 +2,7 @@
  * Author : Jeremy Zhao
  * Email  : jqzhao@live.com
  * Date   : 2015/06/17
- * Update : 2020/09/25
+ * Update : 2020/09/25,2020/11/09
  *
  * Source : https://leetcode.com/problems/maximum-subarray/
  * Problem:	Maximum Subarray
@@ -14,15 +14,12 @@ using std::vector;
 class Solution {
  public: 
    int maxSubArray(vector<int>& nums) {
-     int dp[nums.size()];
-     dp[0] = nums[0];
-     int max = dp[0];
-     for (size_t i = 1; i < nums.size(); ++i) {
-       dp[i] = std::max(nums[i], dp[i - 1] + nums[i]);
-       if (max < dp[i]) {
-         max = dp[i];
-       }
+     int pre = 0;
+     int ans = nums[0];
+     for (int i = 0; i < nums.size(); ++i) {
+       pre = std::max(pre + nums[i], nums[i]);
+       ans = std::max(ans, pre);
      }
-     return max;
+     return ans;
    }
 };
