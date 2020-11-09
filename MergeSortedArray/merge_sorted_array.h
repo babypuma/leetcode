096365b425ebaeecf76207fb428d087b24c8a3a7
Jeremy Zhao <jqzhao@live.com>
2020-11-09 16:12:34 +0800

    updates MergeSortedArray

--- MergeSortedArray/merge_sorted_array.h
+++ MergeSortedArray/merge_sorted_array.h
@@ -2,36 +2,30 @@
  * Author : Jeremy Zhao
  * Email  : jqzhao@live.com
  * Date   : 2014/11/01
+ * Update : 2020/11/09
  *
- * Source : https://oj.leetcode.com/problems/merge-sorted-array/
+ * Source : https://leetcode-cn.com/problems/merge-sorted-array/
  * Problem:	Merge Sorted Array
- * Description: 
- *	Given two sorted integer arrays A and B, merge B into A as one sorted array.  
- * Note:
- *	You may assume that A has enough space (size that is greater or equal to m + n) to hold additional elements 
- *	from B. The number of elements initialized in A and B are m and n respectively.
  *
  */
-class Solution {
-	public:
-	    void merge(int A[], int m, int B[], int n) {
-			int index = m + n - 1;
-			m--;
-			n--;
-			while (m >= 0 && n >= 0) {
-				if (A[m] > B[n]) {
-					A[index] = A[m];
-					m--;
-				}
-				else {
-					A[index] = B[n];
-					n--;
-				} 
-				index--;
-			}
+#include <vector>
+using std::vector;
 
-			while (n >= 0) {
-				A[index--] = B[n--];
-			}
-		}
+class Solution {
+ public:
+   void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+     int i = m + n - 1;
+     int j = m - 1;
+     int k = n - 1;
+     while (j >= 0 && k >= 0) {
+       if (nums1[j] > nums2[k]) {
+         nums1[i--] = nums1[j--];
+       } else {
+         nums1[i--] = nums2[k--];
+       }
+     }
+     while (k >= 0) {
+       nums1[i--] = nums2[k--];
+     }
+   }
 };
--- MergeSortedArray/merge_sorted_array_test.cpp
+++ MergeSortedArray/merge_sorted_array_test.cpp
@@ -2,8 +2,9 @@
  * Author : Jeremy Zhao
  * Email  : jqzhao@live.com
  * Date   : 2014/11/01
+ * Update : 2020/11/09
  *
- * Source : https://oj.leetcode.com/problems/merge-sorted-array/
+ * Source : https://leetcode-cn.com/problems/merge-sorted-array/
  * Problem:	Merge Sorted Array
  *
  */
@@ -11,22 +12,24 @@
 #include "merge_sorted_array.h"
 
 class SolutionTest : public ::testing::Test {
-    protected:
-        virtual void SetUp() {
-        }
-        virtual void TearDown() {
-        }
+ protected:
+   virtual void SetUp() {
+   }
+   virtual void TearDown() {
+   }
 
-        Solution solution_;
+   Solution solution_;
 };
 
 TEST_F(SolutionTest, case1) {
-	int a[8] = {1, 3, 5, 7};
-	int b[] = {2, 4, 6, 8};
-	int c[] = {1, 2, 3, 4, 5, 6, 7, 8};
-	solution_.merge(a, 4, b, 4);
-	for (size_t i = 0; i < sizeof(c)/sizeof(int); i++) {
-		EXPECT_EQ(c[i], a[i]);
-	}
+  int a[] = {1, 3, 5, 7, 0, 0, 0, 0};
+  int b[] = {2, 4, 6, 8};
+  int c[] = {1, 2, 3, 4, 5, 6, 7, 8};
+  vector<int> va(a, a + sizeof(a)/sizeof(a[0]));
+  vector<int> vb(b, b + sizeof(b)/sizeof(b[0]));
+  solution_.merge(va, 4, vb, 4);
+  for (size_t i = 0; i < sizeof(c)/sizeof(c[0]); i++) {
+    EXPECT_EQ(c[i], va[i]);
+  }
 }
 
