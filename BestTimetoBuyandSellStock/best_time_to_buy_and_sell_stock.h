c3908359b74793716895523e116eb23aa4ee6ed4
Jeremy Zhao <jqzhao@live.com>
2020-11-09 19:42:15 +0800

    updates BestTimetoBuyandSellStock

--- BestTimetoBuyandSellStock/best_time_to_buy_and_sell_stock.h
+++ BestTimetoBuyandSellStock/best_time_to_buy_and_sell_stock.h
@@ -2,9 +2,9 @@
  * Author : Jeremy Zhao
  * Email  : jqzhao@live.com
  * Date   : 2014/10/27
- * Update : 2020/10/17
+ * Update : 2020/10/17,2020/11/09
  *
- * Source : https://oj.leetcode.com/problems/best-time-to-buy-and-sell-stock/
+ * Source : https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/
  * Problem:	Best Time to Buy and Sell Stock
  *
  */
@@ -12,26 +12,18 @@
 using std::vector;
 
 class Solution {
-	public:
-		int maxProfit(vector<int> &prices) {
-			int length = prices.size();
-			int max_profit = 0;
-			int min = 0;
-			int max = 0;
-			for (int i = 1; i < length; i++) {
-				if (prices[i] < prices[min]) {
-					min = i;
-					max = i;
-				} else if (prices[i] > prices[max]) {
-					max = i;
-				} 
-
-				if (prices[max] - prices[min] > max_profit) { 
-					max_profit = prices[max] - prices[min]; 
-				}
-			}
-
-			return max_profit;
-		}
+ public:
+   int maxProfit(vector<int> &prices) {
+     if (prices.empty()) {
+       return 0;
+     }
+     int min = prices[0];
+     int ans = 0;
+     for (int i = 1; i < prices.size(); ++i) {
+       ans = std::max(ans, prices[i] - min);
+       min = std::min(min, prices[i]);
+     }
+     return ans;
+   }
 };
 
