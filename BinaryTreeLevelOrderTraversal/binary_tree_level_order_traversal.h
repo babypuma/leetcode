8b85d34fbaad40f9fbc10db66d0ba78bbb1b4e03
Jeremy Zhao <jqzhao@live.com>
2020-11-09 21:00:36 +0800

    updates BinaryTreeLevelOrderTraversal

--- BinaryTreeLevelOrderTraversal/binary_tree_level_order_traversal.h
+++ BinaryTreeLevelOrderTraversal/binary_tree_level_order_traversal.h
@@ -2,12 +2,10 @@
  * Author : Jeremy Zhao
  * Email  : jqzhao@live.com
  * Date   : 2014/10/27
+ * Update : 2020/11/09
  *
- * Source : https://oj.leetcode.com/problems/binary-tree-level-order-traversal/
+ * Source : https://leetcode-cn.com/problems/binary-tree-level-order-traversal/
  * Problem:	Binary Tree Level Order Traversal
- * Description: 
- * 	Given a binary tree, return the level order traversal of its nodes' values. 
- *	(ie, from left to right, level by level).
  *               
  */
 #include <vector>
@@ -23,40 +21,33 @@ struct TreeNode {
 };
 
 class Solution {
-public:
-		vector<vector<int> > levelOrder(TreeNode *root) {
-			vector<vector<int> > vec2d;
-			if (root == NULL) {
-				return vec2d;
-			}
+ public:
+   vector<vector<int> > levelOrder(TreeNode* root) {
+     vector<vector<int> > vec;
+     if (root == NULL) {
+       return vec;
+     }
+     queue<TreeNode*> que;
+     que.push(root);
+     que.push(NULL);
+     vec.push_back(vector<int>());
+     while (!que.empty()) {
+       TreeNode *node = que.front();
+       que.pop();
+       if (node != NULL) {
+         vec.back().push_back(node->val);
+         if (node->left != NULL) {
+           que.push(node->left);
+         }
+         if (node->right != NULL) {
+           que.push(node->right);
+         }
+       } else if (!que.empty()) {
+         que.push(NULL);
+         vec.push_back(vector<int>());
+       }
+     }
 
-			queue<TreeNode *> que;
-			que.push(root);
-			que.push(NULL);
-			vector<int> vec;
-			while (!que.empty()) {
-				TreeNode *node = que.front();	
-				if (node == NULL) {
-					vec2d.push_back(vec);
-					vec.clear();
-
-					que.pop();
-					if (!que.empty()) {
-						que.push(NULL);
-					}
-				}
-				else {
-					vec.push_back(node->val);
-					que.pop();
-					if (node->left != NULL) {
-						que.push(node->left);
-					}
-					if (node->right != NULL) {
-						que.push(node->right);
-					}
-				}
-			}
-
-			return vec2d;
-		}
+     return vec;
+   }
 };
--- BinaryTreeLevelOrderTraversal/binary_tree_level_order_traversal_test.cpp
+++ BinaryTreeLevelOrderTraversal/binary_tree_level_order_traversal_test.cpp
@@ -2,31 +2,23 @@
  * Author : Jeremy Zhao
  * Email  : jqzhao@live.com
  * Date   : 2014/10/27
+ * Update : 2020/11/09
  *
- * Source : https://oj.leetcode.com/problems/binary-tree-level-order-traversal/
+ * Source : https://leetcode-cn.com/problems/binary-tree-level-order-traversal/
  * Problem:	Binary Tree Level Order Traversal
  *               
  */
 #include <gtest/gtest.h>
 #include "binary_tree_level_order_traversal.h"
 
-class SolutionTest : public ::testing::Test {
-    protected:
-        virtual void SetUp() {
-        }
-        virtual void TearDown() {
-        }
-
-        Solution solution_;
-};
-
-TEST_F(SolutionTest, case1) {
+TEST(SolutionTest, levelOrder1) {
 	TreeNode *root = new TreeNode(3);
 	root->left = new TreeNode(9);
 	root->right = new TreeNode(20);
 	root->right->left = new TreeNode(15);
 	root->right->right = new TreeNode(7);
-	vector<vector<int> > vec2d = solution_.levelOrder(root);
+  Solution solution;
+	vector<vector<int> > vec2d = solution.levelOrder(root);
 	EXPECT_EQ((int)vec2d.size(), 3);
 	EXPECT_EQ((int)vec2d[0].size(), 1);
 	EXPECT_EQ(vec2d[0][0], 3);
@@ -38,9 +30,10 @@ TEST_F(SolutionTest, case1) {
 	EXPECT_EQ(vec2d[2][1], 7);
 }
 
-TEST_F(SolutionTest, case2) {
+TEST(SolutionTest, levelOrder2) {
 	TreeNode *root = NULL;
-	vector<vector<int> > vec2d = solution_.levelOrder(root);
+  Solution solution;
+	vector<vector<int> > vec2d = solution.levelOrder(root);
 	EXPECT_EQ((int)vec2d.size(), 0);
 }
 
