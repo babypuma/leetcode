113d8ed6171db61af557f6d06eb77dd43b9b8389
Jeremy Zhao <jqzhao@live.com>
2020-11-09 20:29:44 +0800

    updates ValidPalindrome

--- ValidPalindrome/valid_palindrome.h
+++ ValidPalindrome/valid_palindrome.h
@@ -2,64 +2,32 @@
  * Author : Jeremy Zhao
  * Email  : jqzhao@live.com
  * Date   : 2014/10/30
+ * Update : 2020/11/09
  *
- * Source : https://oj.leetcode.com/problems/valid-palindrome/
+ * Source : https://leetcode-cn.com/problems/valid-palindrome/
  * Problem:	Valid Palindrome 
- * Description: 
- *	Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.  
- *	For example, 
- *	"A man, a plan, a canal: Panama" is a palindrome.  
- *	"race a car" is not a palindrome.
  *
  */
-#include <ctype.h>
 #include <string>
+#include <cctype>
 using std::string;
 
 class Solution {
-	public:
-	    bool isPalindrome(string s) { 
-			int length = s.size();
-			int i = 0;
-			int j = length - 1;
-			while (i < j) {
-				while (i < length && !isalpha(s[i]) && !isdigit(s[i])) {
-					i++;
-				}
-				while (j >= 0 && !isalpha(s[j]) && !isdigit(s[j])) {
-					j--;
-				}
-
-				if (i > j) {
-					break;
-				}
-
-				if (isalpha(s[i]) && isalpha(s[j])) {
-					int n = s[i] - s[j];
-					if (n == 0 || n == GAP || n == -GAP) {
-						i++;
-						j--;
-					}
-					else {
-						return false;
-					}
-				}
-				else if (isdigit(s[i]) && isdigit(s[j])) {
-					if (s[i] == s[j]) {
-						i++;
-						j--;
-					}
-					else {
-						return false;
-					}
-				}
-				else {
-					return false;
-				}
-			}
-
-			return true;
-		}
-
-		static const int GAP = 'A' - 'a';
+ public:
+   bool isPalindrome(string s) {
+     int left = 0;
+     int right = s.size() - 1;
+     while (left < right) {
+       while (left <= right && !isalnum(s[left])) {
+         left++;
+       }
+       while (left <= right && !isalnum(s[right])) {
+         right--;
+       }
+       if (left < right && tolower(s[left++]) != tolower(s[right--])) {
+         return false;
+       }
+     }
+     return true;
+   }
 };
--- ValidPalindrome/valid_palindrome_test.cpp
+++ ValidPalindrome/valid_palindrome_test.cpp
@@ -2,33 +2,20 @@
  * Author : Jeremy Zhao
  * Email  : jqzhao@live.com
  * Date   : 2014/10/30
+ * Update : 2020/11/09
  *
- * Source : https://oj.leetcode.com/problems/valid-palindrome/
+ * Source : https://leetcode-cn.com/problems/valid-palindrome/
  * Problem:	Valid Palindrome 
  *
  */
 #include <gtest/gtest.h>
 #include "valid_palindrome.h"
 
-class SolutionTest : public ::testing::Test {
-    protected:
-        virtual void SetUp() {
-        }
-        virtual void TearDown() {
-        }
-
-        Solution solution_;
-};
-
-TEST_F(SolutionTest, case1) {
-	EXPECT_TRUE(solution_.isPalindrome("A man, a plan, a canal: Panama"));
-}
-
-TEST_F(SolutionTest, case2) {
-	EXPECT_FALSE(solution_.isPalindrome("race a car"));
-}
-
-TEST_F(SolutionTest, case3) {
-	EXPECT_TRUE(solution_.isPalindrome(",."));
+TEST(SolutionTest, isPalindrome) {
+  Solution solution;
+  EXPECT_TRUE(solution.isPalindrome("A man, a plan, a canal: Panama"));
+  EXPECT_FALSE(solution.isPalindrome("race a car"));
+  EXPECT_TRUE(solution.isPalindrome(",."));
+  EXPECT_FALSE(solution.isPalindrome("OP"));
 }
 
