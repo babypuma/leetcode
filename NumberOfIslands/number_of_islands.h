6cb3bf696f1a30956280e36571087a1b00f203f5
Jeremy Zhao <jqzhao@live.com>
2020-11-11 11:52:34 +0800

    updates NumberOfIslands

--- NumberOfIslands/number_of_islands.h
+++ NumberOfIslands/number_of_islands.h
@@ -2,6 +2,7 @@
  * Author : Jeremy Zhao
  * Email  : jqzhao@live.com
  * Date   : 2020/09/09
+ * Update : 2020/11/11
  *
  * Source : https://leetcode.com/problems/number-of-islands/
  * Problem:	Number of Islands
@@ -11,84 +12,84 @@
 #include <vector>
 using std::vector;
 
+class UF {
+ public:
+  UF(vector<vector<char> >& grid) {
+    row_ = grid.size();
+    col_ = (row_ > 0) ? grid[0].size() : 0;
+    num_ = 0;
+    sz_.reserve(row_ * col_);
+    id_.reserve(row_ * col_);
+    for (int i = 0; i < row_; ++i) {
+      for (int j = 0; j < col_; ++j) {
+        int idx = col_ * i + j;
+        if (grid[i][j] == '1') {
+          sz_.push_back(1);
+          ++num_;
+        } else {
+          sz_.push_back(0);
+        }
+        id_.push_back(idx);
+      }
+    }
+  }
+
+  int find(int idx) {
+    int p = id_[idx];
+    while (p != id_[p]) {
+      p = id_[p];
+    }
+    return p;
+  }
+
+  void union_(int a, int b) {
+    int p1 = find(a);
+    int p2 = find(b);
+    if (p1 == p2) {
+      return;
+    }
+    if (sz_[p1] < sz_[p2]) {
+      id_[p1] = p2;
+      sz_[p2] += sz_[p1];
+    } else {
+      id_[p2] = p1;
+      sz_[p1] += sz_[p2];
+    }
+    --num_;
+  }
+
+  int count() {
+    return num_;
+  }
+
+ private:
+  int row_;
+  int col_;
+  int num_;
+  vector<int> sz_;
+  vector<int> id_;
+};
+
 class Solution {
  public:
    int numIslands(vector<vector<char> >& grid) {
-     init(grid);
-     for (size_t i = 0; i < row_; ++i) {
-       for (size_t j = 0; j < column_; ++j) {
-         if (grid[i][j] != '1') {
-           continue;
-         }
-         if (i > 0 && grid[i - 1][j] == '1') {
-           connect(i - 1, j, i, j);
-         }
-         if (j > 0 && grid[i][j - 1] == '1') {
-           connect(i, j - 1, i, j);
-         }
-       }
-     }
-
-     return count();
-   }
-
-   void init(vector<vector<char> >& grid) {
-     row_ = grid.size();
-     column_ = grid.empty() ? 0 : grid[0].size();
-     id_.clear();
-     sz_.clear();
-     id_.resize(row_ * column_);
-     sz_.resize(row_ * column_);
-     count_ = 0;
-     for (size_t i = 0; i < row_; ++i) {
-       for (size_t j = 0; j < column_; ++j) {
-         int idx = getIndex(i, j);
-         id_[idx] = idx;
-         sz_[idx] = 1;
+     UF uf(grid);
+     int row = grid.size();
+     int col = (row > 0) ? grid[0].size() : 0;
+     for (int i = 0; i < row; ++i) {
+       for (int j = 0; j < col; ++j) {
          if (grid[i][j] == '1') {
-           count_++;
+           int idx = i * col + j;
+           if (i > 0 && grid[i - 1][j] == '1') {
+             uf.union_(idx, idx - col);
+           }
+           if (j > 0 && grid[i][j - 1] == '1') {
+             uf.union_(idx, idx - 1);
+           }
          }
        }
      }
-   }
 
- private:
-   int find(int i, int j) {
-     int idx = getIndex(i, j);
-     while (id_[idx] != idx) {
-       idx = id_[idx];
-     }
-     return idx;
+     return uf.count();
    }
-
-   void connect(int i1, int j1, int i2, int j2) {
-     int id1 = find(i1, j1);
-     int id2 = find(i2, j2);
-     if (id1 == id2) {
-       return;
-     }
-     if (sz_[id1] > sz_[id2]) {
-       id_[id2] = id_[id1];
-       sz_[id1] += sz_[id2];
-     } else {
-       id_[id1] = id_[id2];
-       sz_[id2] += sz_[id1];
-     }
-     count_--;
-   }
-
-   int count() {
-     return count_;
-   }
-
-   int getIndex(int i, int j) {
-     return i * column_ + j;
-   }
-
- private:
-   int row_;
-   int column_;
-   int count_;
-   vector<int> id_;
-   vector<int> sz_;
 };
