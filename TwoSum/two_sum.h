ae602ac7ebae5dc1537f8a3e3a6cf8bc601f7be9
Jeremy Zhao <jqzhao@live.com>
2020-11-18 22:19:31 +0800

    updates TwoSum

--- TwoSum/two_sum.h
+++ TwoSum/two_sum.h
@@ -1,125 +1,36 @@
+/*
+ * Author : Jeremy Zhao
+ * Email  : jqzhao@live.com
+ * Date   : 2014/08/31
+ * Update : 2020/11/18
+ *
+ * Source : https://leetcode-cn.com/problems/two-sum/
+ * Problem: Two Sum
+ *               
+ */
 #include <vector>
+#include <unordered_map>
+#include <utility>
 using std::vector;
-
-struct TableElem {
-	int value_;
-	int index_;	
-	TableElem *next_; 
-};
-
-class HashTable {
-	public:
-		HashTable(int size) : hash_table_(NULL), hash_table_length_(0) {
-			init(size);
-		}
-
-		~HashTable() {
-			destroy();
-		}
-
-		int insert(TableElem *elem) {
-			if (!elem) {
-				return 0;
-			}
-			elem->next_ = NULL;
-
-			int pos = hash(elem->value_);
-			TableElem *pelem = hash_table_[pos]; 
-
-			if (!pelem) {
-				hash_table_[pos] = elem;
-			}
-			else {
-				while (pelem->next_ != NULL && pelem->next_->index_ < elem->index_) {
-					pelem = pelem->next_;
-				}
-				elem->next_ = pelem->next_;
-				pelem->next_ = elem;
-			}
-
-			return 0;
-		}
-
-		int find(int value, int cursor) {
-			int pos = hash(value);
-			TableElem *pelem = hash_table_[pos];
-			if (!pelem) {
-				return -1;
-			}
-			else {
-				while (pelem != NULL && pelem->index_ <= cursor) {
-					pelem = pelem->next_;
-				}
-
-				while (pelem != NULL && pelem->value_ != value) {
-					pelem = pelem->next_;
-				}
-
-				return pelem != NULL ? pelem->index_ : -1;
-			}
-		}
-
-	private:
-		void init(int size) {
-			hash_table_length_ = 1;
-			while (hash_table_length_ < size) {
-				hash_table_length_ <<= 1;
-			}
-			hash_table_length_++;
-
-			hash_table_ = (TableElem **)calloc(hash_table_length_, sizeof(TableElem *));
-			assert(hash_table_ != NULL);
-		}
-
-		void destroy() {
-			TableElem *pelem;
-			TableElem *pnext;
-			for (int i = 0; i < hash_table_length_; i++) {
-				pelem = hash_table_[i];	
-				while (pelem != NULL) {
-					pnext = pelem->next_;
-					free(pelem);
-					pelem = pnext;
-				}
-			}
-
-			free(hash_table_);
-		}
-
-		int hash(int value) {
-			return value >= 0 ? value % hash_table_length_ : -value % hash_table_length_;
-		}
-
-	private:
-		TableElem **hash_table_;
-		int hash_table_length_;
-};
+using std::unordered_multimap;
 
 class Solution {
-    public:
-        vector<int> twoSum(vector<int> &numbers, int target) { 
-			size_t length = numbers.size();
-			HashTable hash_table(length << 1);
-			for (size_t i = 0; i < length; i++) {
-				TableElem *elem = (TableElem *)calloc(sizeof(TableElem), 1);
-				elem->value_ = numbers[i];
-				elem->index_ = i + 1;
-				elem->next_ = NULL;
-				hash_table.insert(elem);
-			}
-
-			vector<int> result;
-			int index;
-			for (size_t i = 0; i < length - 1; i++) {
-				index = hash_table.find(target - numbers[i], i + 1); 
-				if (index != -1) {
-					result.push_back(i + 1);
-					result.push_back(index);
-					break;
-				}
-			}
-
-            return result;
-        }
-
+ public:
+   vector<int> twoSum(vector<int> &numbers, int target) { 
+     vector<int> vec;
+     unordered_multimap<int, int> mp;
+     for (int i = 0; i < numbers.size(); ++i) {
+       auto range = mp.equal_range(target - numbers[i]);
+       for (auto it = range.first; it != range.second; ++it) {
+         vec.push_back(it->second);
+         vec.push_back(i);
+         break;
+       }
+       if (!vec.empty()) {
+         break;
+       }
+       mp.insert(std::pair<int, int>(numbers[i], i));
+     }
+     return vec;
+   }
 };
--- TwoSum/two_sum_test.cpp
+++ TwoSum/two_sum_test.cpp
@@ -1,41 +1,51 @@
+/*
+ * Author : Jeremy Zhao
+ * Email  : jqzhao@live.com
+ * Date   : 2014/08/31
+ * Update : 2020/11/18
+ *
+ * Source : https://leetcode-cn.com/problems/two-sum/
+ * Problem:	Two Sum
+ *               
+ */
 #include <gtest/gtest.h>
 #include "two_sum.h"
 
 class SolutionTest : public ::testing::Test {
-    protected:
-        virtual void SetUp() {
-        }
-        virtual void TearDown() {
-        }
+ protected:
+   virtual void SetUp() {
+   }
+   virtual void TearDown() {
+   }
 
-        Solution solution_;
+   Solution solution_;
 };
 
 TEST_F(SolutionTest, hashTable) {
-	vector<int> result;
+  vector<int> result;
 
-    vector<int> v;
-	v.push_back(2);
-	v.push_back(7);
-	v.push_back(11);
-	v.push_back(15);
-	result = solution_.twoSum(v, 9);
-	EXPECT_EQ(result[0], 1);
-	EXPECT_EQ(result[1], 2);
+  vector<int> v;
+  v.push_back(2);
+  v.push_back(7);
+  v.push_back(11);
+  v.push_back(15);
+  result = solution_.twoSum(v, 9);
+  EXPECT_EQ(result[0], 0);
+  EXPECT_EQ(result[1], 1);
 
-	v.clear();
-	v.push_back(-3);
-	v.push_back(4);
-	v.push_back(3);
-	v.push_back(15);
-	result = solution_.twoSum(v, 0);
-	EXPECT_EQ(result[0], 1);
-	EXPECT_EQ(result[1], 3);
+  v.clear();
+  v.push_back(-3);
+  v.push_back(4);
+  v.push_back(3);
+  v.push_back(15);
+  result = solution_.twoSum(v, 0);
+  EXPECT_EQ(result[0], 0);
+  EXPECT_EQ(result[1], 2);
 
-	int a[] = {230,863,916,585,981,404,316,785,88,12,70,435,384,778,887,755,740,337,86,92,325,422,815,650,920,125,277,336,221,847,168,23,677,61,400,136,874,363,394,199,863,997,794,587,124,321,212,957,764,173,314,422,927,783,930,282,306,506,44,926,691,568,68,730,933,737,531,180,414,751,28,546,60,371,493,370,527,387,43,541,13,457,328,227,652,365,430,803,59,858,538,427,583,368,375,173,809,896,370,789};
-	vector<int> vec(a, a + sizeof(a) / sizeof(int));
-	result = solution_.twoSum(vec, 542);
-	EXPECT_EQ(result[0], 29);
-	EXPECT_EQ(result[1], 46);
+  int a[] = {230,863,916,585,981,404,316,785,88,12,70,435,384,778,887,755,740,337,86,92,325,422,815,650,920,125,277,336,221,847,168,23,677,61,400,136,874,363,394,199,863,997,794,587,124,321,212,957,764,173,314,422,927,783,930,282,306,506,44,926,691,568,68,730,933,737,531,180,414,751,28,546,60,371,493,370,527,387,43,541,13,457,328,227,652,365,430,803,59,858,538,427,583,368,375,173,809,896,370,789};
+  vector<int> vec(a, a + sizeof(a) / sizeof(int));
+  result = solution_.twoSum(vec, 542);
+  EXPECT_EQ(result[0], 28);
+  EXPECT_EQ(result[1], 45);
 }
 
