0a9c13d8098bf3ad3bba7bea0639c527190b3719
Jeremy Zhao <jqzhao@live.com>
2020-12-05 12:47:22 +0800

    updates MedianofTwoSortedArrays

--- MedianofTwoSortedArrays/median_of_two_sorted_arrays.h
+++ MedianofTwoSortedArrays/median_of_two_sorted_arrays.h
@@ -1,36 +1,45 @@
+/*
+ * Author : Jeremy Zhao
+ * Email  : jqzhao@live.com
+ * Date   : 2015/03/18
+ * Update : 2020/12/05
+ *
+ * Source : https://leetcode-cn.com/problems/median-of-two-sorted-arrays/
+ * Problem:	Median of Two Sorted Arrays
+ *               
+ */
 #include <vector>
 using std::vector;
 
 class Solution {
-	public:
-	    double findMedianSortedArrays(int A[], int m, int B[], int n) { 
-			vector<int> merged_array;
-			int i = 0;
-			int j = 0;
-			while (i < m && j < n) {
-				if (A[i] < B[j]) {
-					merged_array.push_back(A[i++]);
-				}
-				else {
-					merged_array.push_back(B[j++]);
-				} 
-			}
+ public:
+   double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+     if (nums1.size() > nums2.size()) {
+       return findMedianSortedArrays(nums2, nums1);
+     }
 
-			if (i == m) {
-				while (j < n) {
-					merged_array.push_back(B[j++]);
-				}
-			}
-			else if (j == n) {
-				while (i < m) {
-					merged_array.push_back(A[i++]);
-				}
-			}
+     int m = nums1.size();
+     int n = nums2.size();
+     int left = 0;
+     int right = m;
+     int median1 = 0;
+     int median2 = 0;
+     while (left <= right) {
+       int i = (left + right) / 2;
+       int pre_i = (i == 0) ? INT_MIN : nums1[i - 1];
+       int elem_i = (i == m) ? INT_MAX : nums1[i];
+       int j = (m + n + 1) / 2 - i;
+       int pre_j = (j == 0) ? INT_MIN : nums2[j - 1];
+       int elem_j = (j == n) ? INT_MAX : nums2[j];
+       if (pre_i <= elem_j) {
+         left = i + 1;
+         median1 = std::max(pre_i, pre_j);
+         median2 = std::min(elem_i, elem_j);
+       } else {
+         right = i - 1;
+       }
+     }
 
-			if ((m + n) % 2 == 0) {
-				return ((double)merged_array[(m + n)/2 - 1] + merged_array[(m + n)/2])/2;
-			}
-
-			return (double)merged_array[(m + n)/2];
-		}
+     return ((m + n) % 2 == 0) ? (median1 + median2) / 2.0 : median1;
+   }
 };
--- MedianofTwoSortedArrays/median_of_two_sorted_arrays_test.cpp
+++ MedianofTwoSortedArrays/median_of_two_sorted_arrays_test.cpp
@@ -1,67 +1,70 @@
+/*
+ * Author : Jeremy Zhao
+ * Email  : jqzhao@live.com
+ * Date   : 2015/03/18
+ * Update : 2020/12/05
+ *
+ * Source : https://leetcode-cn.com/problems/median-of-two-sorted-arrays/
+ * Problem:	Median of Two Sorted Arrays
+ *               
+ */
 #include <gtest/gtest.h>
 #include "median_of_two_sorted_arrays.h"
 
 class SolutionTest : public ::testing::Test {
-    protected:
-        virtual void SetUp() {
-        }
-        virtual void TearDown() {
-        }
+ protected:
+   virtual void SetUp() {
+   }
+   virtual void TearDown() {
+   }
 
-        Solution solution_;
+   Solution solution_;
 };
 
 TEST_F(SolutionTest, case1) {
-	int A[] = {1, 3, 5, 7, 9};
-	int B[] = {2, 4, 6, 8, 10};
-	EXPECT_DOUBLE_EQ(solution_.findMedianSortedArrays(A, sizeof(A)/sizeof(int), B, sizeof(B)/sizeof(int)), 5.5);
-}
-
-TEST_F(SolutionTest, case2) {
-	int A[] = {1, 3, 5, 7, 9};
-	int B[] = {2, 4, 6, 8, 10};
-	EXPECT_DOUBLE_EQ(solution_.findMedianSortedArrays(A, 0, B, sizeof(B)/sizeof(int)), 6.0);
-}
-
-TEST_F(SolutionTest, case3) {
-	int A[] = {1, 3, 5, 7, 9};
-	int B[] = {2, 4, 6, 8, 10};
-	EXPECT_DOUBLE_EQ(solution_.findMedianSortedArrays(A, sizeof(A)/sizeof(int), B, 0), 5.0);
+  int A[] = {1, 3, 5, 7, 9};
+  int B[] = {2, 4, 6, 8, 10};
+  vector<int> v1(A, A + sizeof(A)/sizeof(A[0]));
+  vector<int> v2(B, B + sizeof(B)/sizeof(B[0]));
+  EXPECT_DOUBLE_EQ(solution_.findMedianSortedArrays(v1, v2), 5.5);
 }
 
 TEST_F(SolutionTest, case4) {
-	int A[] = {1, 3, 5};
-	int B[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
-	EXPECT_DOUBLE_EQ(solution_.findMedianSortedArrays(A, sizeof(A)/sizeof(int), B, sizeof(B)/sizeof(int)), 8.0);
+  int A[] = {1, 3, 5};
+  int B[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+  vector<int> v1(A, A + sizeof(A)/sizeof(A[0]));
+  vector<int> v2(B, B + sizeof(B)/sizeof(B[0]));
+  EXPECT_DOUBLE_EQ(solution_.findMedianSortedArrays(v1, v2), 8.0);
 }
 
 TEST_F(SolutionTest, case5) {
-	int A[] = {1, 3, 5};
-	int B[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22};
-	EXPECT_DOUBLE_EQ(solution_.findMedianSortedArrays(A, sizeof(A)/sizeof(int), B, sizeof(B)/sizeof(int)), 9.0);
+  int A[] = {1, 3, 5};
+  int B[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22};
+  vector<int> v1(A, A + sizeof(A)/sizeof(A[0]));
+  vector<int> v2(B, B + sizeof(B)/sizeof(B[0]));
+  EXPECT_DOUBLE_EQ(solution_.findMedianSortedArrays(v1, v2), 9.0);
 }
 
 TEST_F(SolutionTest, case6) {
-	int A[] = {1, 3, 5, 7, 9};
-	int B[] = {2, 4, 6, 8, 10, 12};
-	EXPECT_DOUBLE_EQ(solution_.findMedianSortedArrays(A, 0, B, sizeof(B)/sizeof(int)), 7.0);
-}
-
-TEST_F(SolutionTest, case7) {
-	int A[] = {1, 3, 5, 7, 9};
-	int B[] = {2, 4, 6, 8, 10, 12};
-	EXPECT_DOUBLE_EQ(solution_.findMedianSortedArrays(A, 0, B, 2), 3.0);
+  int A[] = {1, 3, 5, 7, 9};
+  int B[] = {2, 4, 6, 8, 10, 12};
+  vector<int> v1(A, A + sizeof(A)/sizeof(A[0]));
+  vector<int> v2(B, B + sizeof(B)/sizeof(B[0]));
+  EXPECT_DOUBLE_EQ(solution_.findMedianSortedArrays(v1, v2), 6.0);
 }
 
 TEST_F(SolutionTest, case8) {
-	int A[] = {2, 2, 2, 2};
-	int B[] = {2, 2, 2};
-	EXPECT_DOUBLE_EQ(solution_.findMedianSortedArrays(A, sizeof(A)/sizeof(int), B, sizeof(B)/sizeof(int)), 2.0);
+  int A[] = {2, 2, 2, 2};
+  int B[] = {2, 2, 2};
+  vector<int> v1(A, A + sizeof(A)/sizeof(A[0]));
+  vector<int> v2(B, B + sizeof(B)/sizeof(B[0]));
+  EXPECT_DOUBLE_EQ(solution_.findMedianSortedArrays(v1, v2), 2.0);
 }
 
 TEST_F(SolutionTest, case9) {
-	int A[] = {3};
-	int B[] = {1, 2};
-	EXPECT_DOUBLE_EQ(solution_.findMedianSortedArrays(A, sizeof(A)/sizeof(int), B, sizeof(B)/sizeof(int)), 2.0);
+  int A[] = {3};
+  int B[] = {1, 2};
+  vector<int> v1(A, A + sizeof(A)/sizeof(A[0]));
+  vector<int> v2(B, B + sizeof(B)/sizeof(B[0]));
+  EXPECT_DOUBLE_EQ(solution_.findMedianSortedArrays(v1, v2), 2.0);
 }
-
