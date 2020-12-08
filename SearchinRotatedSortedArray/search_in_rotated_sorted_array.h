aec169048764080176862b135cdf37f7a65bcd72
Jeremy Zhao <jqzhao@live.com>
2020-12-08 22:17:01 +0800

    updates SearchinRotatedSortedArray

--- SearchinRotatedSortedArray/search_in_rotated_sorted_array.h
+++ SearchinRotatedSortedArray/search_in_rotated_sorted_array.h
@@ -2,62 +2,55 @@
  * Author : Jeremy Zhao
  * Email  : jqzhao@live.com
  * Date   : 2014/11/12
+ * Update : 2020/12/08
  *
- * Source : https://oj.leetcode.com/problems/search-in-rotated-sorted-array/
+ * Source : https://leetcode-cn.com/problems/search-in-rotated-sorted-array/
  * Problem:	Search in Rotated Sorted Array 
- * Description: 
- *	Suppose a sorted array is rotated at some pivot unknown to you beforehand.  
- *	(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
- *	You are given a target value to search. If found in the array return its index, otherwise return -1.
- *  You may assume no duplicate exists in the array.
  *
  */
-class Solution {
-	public:
-	    int search(int A[], int n, int target) {
-			return binarySearch(A, 0, n - 1, target);
-		}
+#include <vector>
+using std::vector;
 
-		int binarySearch(int A[], int left, int right, int target) {
-			int middle;
-			while (left <= right) {
-				middle = (left + right) / 2;
-				if (target == A[middle]) {
-					return middle;
-				}
-				if (target < A[middle]) {
-					if (target == A[right]) {
-						return right;
-					}
-					if (A[middle] <= A[right]) {
-						right = middle - 1;
-					}
-					else {
-						if (target < A[right]) {
-							left = middle + 1;
-						}
-						else {
-							right = middle - 1;
-						}
-					}
-				}
-				else {
-					if (target == A[left]) {
-						return left;
-					}
-					if (A[middle] >= A[left]) {
-						left = middle + 1;
-					}
-					else {
-						if (target > A[left]) {
-							right = middle - 1;
-						}
-						else {
-							left = middle + 1;
-						}
-					}
-				}
-			}
-			return -1;
-		}
+class Solution {
+ public:
+   int search(vector<int>& nums, int target) {
+     return binarySearch(nums, 0, nums.size() - 1, target);
+   }
+
+ private:
+   int binarySearch(const vector<int>& nums, int left, int right, int target) {
+     if (left <= right) {
+       int mid = (left + right) / 2;
+       if (target == nums[mid]) {
+         return mid;
+       }
+
+       if (nums[left] < nums[right]) {
+         if (target > nums[mid]) {
+           return binarySearch(nums, mid + 1, right, target);
+         } else {
+           return binarySearch(nums, left, mid - 1, target);
+         }
+       }
+
+       // nums[left] > nums[right]
+       if (nums[left] < nums[mid]) {
+         if (target >= nums[left] && target < nums[mid]) {
+           return binarySearch(nums, left, mid - 1, target);
+         } else {
+           return binarySearch(nums, mid + 1, right, target);
+         }
+       } else if (nums[left] > nums[mid]) {
+         if (target > nums[mid] && target <= nums[right]) {
+           return binarySearch(nums, mid + 1, right, target);
+         } else {
+           return binarySearch(nums, left, mid - 1, target);
+         }
+       }
+
+       // nums[left] == nums[mid]
+       return binarySearch(nums, mid + 1, right, target);
+     }
+     return -1;
+   }
 };
