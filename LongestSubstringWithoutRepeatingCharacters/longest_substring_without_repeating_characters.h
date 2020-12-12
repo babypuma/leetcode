20e415d1f29f8b14bad40a3577810fc5e0d4712a
Jeremy Zhao <jqzhao@live.com>
2020-12-12 19:45:14 +0800

    updates LongestSubstringWithoutRepeatingCharacters

--- LongestSubstringWithoutRepeatingCharacters/longest_substring_without_repeating_characters.h
+++ LongestSubstringWithoutRepeatingCharacters/longest_substring_without_repeating_characters.h
@@ -1,39 +1,40 @@
+/*
+ * Author : Jeremy Zhao
+ * Email  : jqzhao@live.com
+ * Date   : 2015/03/18
+ * Update : 2020/12/12
+ *
+ * Source : https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
+ * Problem:	Longest Substring Without Repeating Characters
+ *               
+ */
 #include <string>
+#include <vector>
 using std::string;
+using std::vector;
 
 class Solution {
-	public:
-	    int lengthOfLongestSubstring(string s) {
-			int bitmap[256];
-			for (int j = 0; j < 256; j++) {
-				bitmap[j] = 0;
-			}
+ public:
+   int lengthOfLongestSubstring(string s) {
+     vector<int> char_set(256, -1);
+     int ans = 0;
+     int start = 0;
+     int i = 0;
+     while (i < s.size()) {
+       while (i < s.size() && char_set[s[i]] < 0) {
+         char_set[s[i]] = i;
+         ++i;
+       }
+       ans = std::max(ans, i - start);
 
-			size_t max = 0;
-			size_t begin = 0;
-			size_t i = 0;
-			size_t length = s.size();
-			while (i < length) {
-				if (bitmap[s[i]] > 0) {
-					if (i - begin > max) {
-						max = i - begin;
-					}
-					begin = bitmap[s[i]];
-					for (int j = 0; j < 256; j++) {
-						bitmap[j] = 0;
-					}
-					i = begin;
-				}
-				else {
-					bitmap[s[i]] = i + 1;
-					i++;
-				}
-			}
-
-			if (length - begin > max) {
-				max = length - begin;
-			}
-
-			return max;
-		}
+       if (i < s.size()) {
+         int last_pos = char_set[s[i]];
+         for (int j = start; j <= last_pos; ++j) {
+           char_set[s[j]] = -1;
+         }
+         start = last_pos + 1;
+       }
+     }
+     return ans;
+   }
 };
--- LongestSubstringWithoutRepeatingCharacters/longest_substring_without_repeating_characters_test.cpp
+++ LongestSubstringWithoutRepeatingCharacters/longest_substring_without_repeating_characters_test.cpp
@@ -1,25 +1,34 @@
+/*
+ * Author : Jeremy Zhao
+ * Email  : jqzhao@live.com
+ * Date   : 2015/03/18
+ *
+ * Source : https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
+ * Problem:	Longest Substring Without Repeating Characters
+ *               
+ */
 #include <gtest/gtest.h>
 #include "longest_substring_without_repeating_characters.h"
 
 class SolutionTest : public ::testing::Test {
-    protected:
-        virtual void SetUp() {
-        }
-        virtual void TearDown() {
-        }
+ protected:
+   virtual void SetUp() {
+   }
+   virtual void TearDown() {
+   }
 
-        Solution solution_;
+   Solution solution_;
 };
 
 TEST_F(SolutionTest, case1) {
-	EXPECT_EQ(solution_.lengthOfLongestSubstring("abcabcbb"), 3);
+  EXPECT_EQ(solution_.lengthOfLongestSubstring("abcabcbb"), 3);
 }
 
 TEST_F(SolutionTest, case2) {
-	EXPECT_EQ(solution_.lengthOfLongestSubstring("bbbb"), 1);
+  EXPECT_EQ(solution_.lengthOfLongestSubstring("bbbb"), 1);
 }
 
 TEST_F(SolutionTest, case3) {
-	EXPECT_EQ(solution_.lengthOfLongestSubstring("qopubjguxhxdipfzwswybgfylqvjzhar"), 12);
+  EXPECT_EQ(solution_.lengthOfLongestSubstring("qopubjguxhxdipfzwswybgfylqvjzhar"), 12);
 }
 
