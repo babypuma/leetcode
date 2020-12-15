edecbb256e0da5df83e669194c89b6ac8944ba7b
Jeremy Zhao <jqzhao@live.com>
2020-12-16 03:33:34 +0800

    updates LengthOfLastWord

--- LengthofLastWord/length_of_last_word.h
+++ LengthofLastWord/length_of_last_word.h
@@ -2,41 +2,30 @@
  * Author : Jeremy Zhao
  * Email  : jqzhao@live.com
  * Date   : 2014/11/12
+ * Update : 2020/12/16
  *
- * Source : https://oj.leetcode.com/problems/length-of-last-word/
+ * Source : https://leetcode-cn.com/problems/length-of-last-word/
  * Problem:	Length of Last Word
- * Description: 
- *	Given a string s consists of upper/lower-case alphabets and empty space characters ' ', 
- *	return the length of last word in the string.  
- *	If the last word does not exist, return 0.  
- * Note: 
- *	A word is defined as a character sequence consists of non-space characters only.  
- *	For example, 
- *	Given s = "Hello World",
- *	return 5.
  *
  */
+#include <string>
+using std::string;
+
 class Solution {
-	public:
-	    int lengthOfLastWord(const char *s) {
-			int length = 0;
-			int curr_length = 0;
-			for (const char *p = s; *p != '\0'; p++) {
-				while (*(p + curr_length) != '\0' && *(p + curr_length) != ' ') {
-					curr_length++;
-				}
-				if (*(p + curr_length) == '\0') {
-					length = curr_length;
-					break;
-				}
+ public:
+   int lengthOfLastWord(string s) {
+     int j = s.size() - 1;
+     while (j >= 0 && s[j] == ' ') {
+       --j;
+     }
+     if (j < 0) {
+       return 0;
+     }
 
-				if (curr_length > 0) {
-					p = p + curr_length;
-					length = curr_length;
-					curr_length = 0;
-				}
-			}
-
-			return length;
-		}
+     int i = j;
+     while (i >= 0 && s[i] != ' ') {
+       --i;
+     }
+     return j - i;
+   }
 };
--- LengthofLastWord/length_of_last_word_test.cpp
+++ LengthofLastWord/length_of_last_word_test.cpp
@@ -2,8 +2,9 @@
  * Author : Jeremy Zhao
  * Email  : jqzhao@live.com
  * Date   : 2014/11/12
+ * Update : 2020/12/16
  *
- * Source : https://oj.leetcode.com/problems/length-of-last-word/
+ * Source : https://leetcode-cn.com/problems/length-of-last-word/
  * Problem:	Length of Last Word
  *
  */
@@ -11,28 +12,28 @@
 #include "length_of_last_word.h"
 
 class SolutionTest : public ::testing::Test {
-    protected:
-        virtual void SetUp() {
-        }
-        virtual void TearDown() {
-        }
+ protected:
+   virtual void SetUp() {
+   }
+   virtual void TearDown() {
+   }
 
-        Solution solution_;
+   Solution solution_;
 };
 
 TEST_F(SolutionTest, case1) {
-	EXPECT_EQ(solution_.lengthOfLastWord("Hello world"), 5);
+  EXPECT_EQ(solution_.lengthOfLastWord("Hello world"), 5);
 }
 
 TEST_F(SolutionTest, case2) {
-	EXPECT_EQ(solution_.lengthOfLastWord("Hello world "), 5);
+  EXPECT_EQ(solution_.lengthOfLastWord("Hello world "), 5);
 }
 
 TEST_F(SolutionTest, case3) {
-	EXPECT_EQ(solution_.lengthOfLastWord(""), 0);
+  EXPECT_EQ(solution_.lengthOfLastWord(""), 0);
 }
 
 TEST_F(SolutionTest, case4) {
-	EXPECT_EQ(solution_.lengthOfLastWord("a "), 1);
+  EXPECT_EQ(solution_.lengthOfLastWord("a "), 1);
 }
 
