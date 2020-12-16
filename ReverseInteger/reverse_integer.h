db8faaa9890f3e727c5ec249cad01479705cbc7c
Jeremy Zhao <jqzhao@live.com>
2020-12-16 15:30:13 +0800

    updates ReverseInteger

--- ReverseInteger/reverse_integer.h
+++ ReverseInteger/reverse_integer.h
@@ -1,30 +1,40 @@
+/*
+ * Author : Jeremy Zhao
+ * Email  : jqzhao@live.com
+ * Date   : 2015/03/18
+ * Update : 2020/12/16
+ *
+ * Source : https://leetcode-cn.com/problems/reverse-integer/
+ * Problem: Reverse Integer
+ *               
+ */
 #include <vector>
 using std::vector;
 
 class Solution {
-	public:
-	    int reverse(int x) {
-			if (x == 0) {
-				return 0;
-			}
+ public:
+   int reverse(int x) {
+     if (x == 0 || x == INT32_MIN) {
+       return 0;
+     }
 
-			int sign = x > 0 ? 1 : -1;
-			if (x < 0) {
-				x = -x;
-			}
+     int sign = x > 0 ? 1 : -1;
+     if (x < 0) {
+       x = -x;
+     }
 
-			vector<int> bits;
-			while (x > 0) {
-				bits.push_back(x%10);
-				x /= 10;
-			}
+     vector<int> bits;
+     while (x > 0) {
+       bits.push_back(x % 10);
+       x /= 10;
+     }
 
-			int y = 0;
-			for (vector<int>::iterator it = bits.begin(); it != bits.end(); it++) {
-				y *= 10;
-				y += *it;
-			}
+     int64_t y = 0;
+     for (auto& i : bits) {
+       y *= 10;
+       y += i;
+     }
 
-			return y * sign;
-		}
+     return y > INT32_MAX ? 0 : y * sign;
+   }
 };
--- ReverseInteger/reverse_integer_test.cpp
+++ ReverseInteger/reverse_integer_test.cpp
@@ -1,19 +1,30 @@
+/*
+ * Author : Jeremy Zhao
+ * Email  : jqzhao@live.com
+ * Date   : 2015/03/18
+ * Update : 2020/12/16
+ *
+ * Source : https://leetcode-cn.com/problems/reverse-integer/
+ * Problem: Reverse Integer
+ *               
+ */
 #include <gtest/gtest.h>
 #include "reverse_integer.h"
 
 class SolutionTest : public ::testing::Test {
-    protected:
-        virtual void SetUp() {
-        }
-        virtual void TearDown() {
-        }
+ protected:
+   virtual void SetUp() {
+   }
+   virtual void TearDown() {
+   }
 
-        Solution solution_;
+   Solution solution_;
 };
 
 TEST_F(SolutionTest, case1) {
-	EXPECT_EQ(solution_.reverse(0), 0);
-	EXPECT_EQ(solution_.reverse(123), 321);
-	EXPECT_EQ(solution_.reverse(-2310), -132);
-	EXPECT_EQ(solution_.reverse(1000), 1);
+  EXPECT_EQ(solution_.reverse(0), 0);
+  EXPECT_EQ(solution_.reverse(123), 321);
+  EXPECT_EQ(solution_.reverse(-2310), -132);
+  EXPECT_EQ(solution_.reverse(1000), 1);
+  EXPECT_EQ(solution_.reverse(1534236469), 0);
 }
