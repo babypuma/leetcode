441adfc2b9c1aef6b447ed774a196dde7112599c
Jeremy Zhao <jqzhao@live.com>
2020-12-16 18:24:31 +0800

    updates RemoveElement

--- RemoveElement/remove_element.h
+++ RemoveElement/remove_element.h
@@ -1,20 +1,34 @@
-class Solution {
-	public:
-	    int removeElement(int A[], int n, int elem) { 
-			int back = -1;
-			int front = 0;
-			for (; front < n; front++) {
-				if (A[front] != elem) {
-					if (back >= 0) {
-						A[back++] = A[front];
-					}
-					continue;
-				}
-				if (back < 0) {
-					back = front;
-				}
-			}
+/*
+ * Author : Jeremy Zhao
+ * Email  : jqzhao@live.com
+ * Date   : 2015/03/18
+ * Date   : 2020/12/16
+ *
+ * Source : https://leetcode-cn.com/problems/remove-element/
+ * Problem:	Remove Element
+ *               
+ */
+#include <vector>
+using std::vector;
 
-			return (back < 0) ? n : back;
-		}
+class Solution {
+ public:
+   int removeElement(vector<int>& nums, int val) {
+     size_t back = 0;
+     size_t front = 0;
+     while (front < nums.size()) {
+       if (nums[front] == val) {
+         ++front;
+         continue;
+       }
+
+       if (back != front) {
+         nums[back] = nums[front];
+       }
+       ++front;
+       ++back;
+     }
+
+     return back;
+   }
 };
--- RemoveElement/remove_element_test.cpp
+++ RemoveElement/remove_element_test.cpp
@@ -1,38 +1,47 @@
+/*
+ * Author : Jeremy Zhao
+ * Email  : jqzhao@live.com
+ * Date   : 2015/03/18
+ * Date   : 2020/12/16
+ *
+ * Source : https://leetcode-cn.com/problems/remove-element/
+ * Problem:	Remove Element
+ *               
+ */
 #include <gtest/gtest.h>
 #include "remove_element.h"
 
-using std::set;
-
 class SolutionTest : public ::testing::Test {
-    protected:
-        virtual void SetUp() {
-        }
-        virtual void TearDown() {
-        }
+ protected:
+   virtual void SetUp() {
+   }
+   virtual void TearDown() {
+   }
 
-        Solution solution_;
+   Solution solution_;
 };
 
 TEST_F(SolutionTest, case1) {
-	int a[] = {1, 2, 2, 3, 4, 5};
-	int length = solution_.removeElement(a, sizeof(a)/sizeof(int), 2);
+  int a[] = {1, 2, 2, 3, 4, 5};
+  vector<int> vec(a, a + sizeof(a)/sizeof(a[0]));
+  int length = solution_.removeElement(vec, 2);
 
-	int b[] = {1, 3, 4, 5};
-	ASSERT_EQ((int)sizeof(b)/sizeof(int), length);
-	for (size_t i = 0; i < sizeof(b)/sizeof(int); i++) {
-		EXPECT_EQ(a[i], b[i]);
-	}
+  int b[] = {1, 3, 4, 5};
+  ASSERT_EQ((int)sizeof(b)/sizeof(int), length);
+  for (size_t i = 0; i < length; i++) {
+    EXPECT_EQ(vec[i], b[i]);
+  }
 }
 
 TEST_F(SolutionTest, case2) {
-	int a[] = {1, 2, 2, 3, 4, 5};
-	int length = solution_.removeElement(a, sizeof(a)/sizeof(int), 5);
+  int a[] = {1, 2, 2, 3, 4, 5};
+  vector<int> vec(a, a + sizeof(a)/sizeof(a[0]));
+  int length = solution_.removeElement(vec, 5);
 
-	int b[] = {1, 2, 2, 3, 4};
-	ASSERT_EQ((int)sizeof(b)/sizeof(int), length);
-	for (size_t i = 0; i < sizeof(b)/sizeof(int); i++) {
-		EXPECT_EQ(a[i], b[i]);
-	}
-
+  int b[] = {1, 2, 2, 3, 4};
+  ASSERT_EQ((int)sizeof(b)/sizeof(int), length);
+  for (size_t i = 0; i < length; i++) {
+    EXPECT_EQ(vec[i], b[i]);
+  }
 }
 
