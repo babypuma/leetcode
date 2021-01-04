2b4a7d33e3d6d6fb1c65d262fbb1ec479d663dca
Jeremy Zhao <jqzhao@live.com>
2021-01-04 13:15:23 +0800

    updates PartitionList

--- PartitionList/partition_list.h
+++ PartitionList/partition_list.h
@@ -2,70 +2,55 @@
  * Author : Jeremy Zhao
  * Email  : jqzhao@live.com
  * Date   : 2014/11/01
+ * Update : 2021/01/04
  *
- * Source : https://oj.leetcode.com/problems/partition-list/
+ * Source : https://leetcode-cn.com/problems/partition-list/
  * Problem:	Partition List
- * Description: 
- *	Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.  
- *	You should preserve the original relative order of the nodes in each of the two partitions.
- * 	For example,
- * 	Given 1->4->3->2->5->2 and x = 3,
- *	return 1->2->2->4->3->5.
  *
  */
-#include <stdio.h>
-
 struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
 };
 
 class Solution {
-	public:
-		ListNode *partition(ListNode *head, int x) {
-			ListNode *less_head = NULL;
-			ListNode *less_tail = NULL;
-			ListNode *prev = NULL;
-			ListNode *node = head;
-			while (node != NULL && node->val < x) {
-				if (less_head == NULL) {
-					less_head = node;
-				} 
-				less_tail = node;
-				node = node->next;
-			}
+ public:
+   ListNode *partition(ListNode *head, int x) {
+     ListNode* front_head = NULL;
+     ListNode* front_tail = NULL;
+     ListNode* back_head = NULL;
+     ListNode* back_tail = NULL;
 
-			if (node == NULL) {
-				return less_head;
-			}
+     ListNode* curr = head;
+     while (curr != NULL) {
+       if (curr->val < x) {
+         if (front_head == NULL) {
+           front_head = curr;
+         } else {
+           front_tail->next = curr;
+         }
+         front_tail = curr;
+       } else {
+         if (back_head == NULL) {
+           back_head = curr;
+         } else {
+           back_tail->next = curr;
+         }
+         back_tail = curr;
+       }
+       curr = curr->next;
+     }
 
-			head = node;
-			prev = head;
-			node = head->next;
-			while (node != NULL) {
-				if (node->val < x) {
-					if (less_tail != NULL) {
-						less_tail->next = node;
-					}
-					else {
-						less_head = node;
-					} 
-					less_tail = node;
-					prev->next = node->next; 
-				} 
-				else {
-					prev = node;
-				}
+     if (front_head != NULL) {
+       front_tail->next = back_head;
+     } else {
+       front_head = back_head;
+     }
+     if (back_tail != NULL) {
+       back_tail->next = NULL;
+     }
 
-				node = node->next;
-			} 
-
-			if (less_tail != NULL) {
-				less_tail->next = head;
-				return less_head;
-			}
-
-			return head;
-		}
+     return front_head;
+   }
 };
