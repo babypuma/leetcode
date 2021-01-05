68b2b94761c54f9e8fe8177965fae4510ab922f9
Jeremy Zhao <jqzhao@live.com>
2021-01-05 18:13:36 +0800

    updates LetterCombinationsofaPhoneNumber

--- LetterCombinationsofaPhoneNumber/letter_combinations_of_a_phone_number.h
+++ LetterCombinationsofaPhoneNumber/letter_combinations_of_a_phone_number.h
@@ -2,9 +2,11 @@
  * Author : Jeremy Zhao
  * Email  : jqzhao@live.com
  * Date   : 2020/06/30
+ * Update : 2021/01/05
  *
- * Source : https://leetcode.com/problems/letter-combinations-of-a-phone-number
+ * Source : https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/
  * Problem: Letter Combinations of a Phone Number
+ *
  */
 #include <vector>
 #include <string>
@@ -13,44 +15,33 @@ using std::string;
 
 class Solution {
  public:
-   Solution() {
-     digit2letter[2] = "abc";
-     digit2letter[3] = "def";
-     digit2letter[4] = "ghi";
-     digit2letter[5] = "jkl";
-     digit2letter[6] = "mno";
-     digit2letter[7] = "pqrs";
-     digit2letter[8] = "tuv";
-     digit2letter[9] = "wxyz";
-   }
-
    vector<string> letterCombinations(string digits) {
-     solution_.clear();
-     if (digits.empty()) {
-       return solution_;
-     }
-     string solution;
-     backtrack(0, digits, solution);
-     return solution_;
-   }
+     string letter_table[] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+     vector<string> keyboard(letter_table, letter_table + sizeof(letter_table)/sizeof(letter_table[0]));
 
-   void backtrack(int start, const string& source, string& solution) {
-     if (solution.size() == source.size()) {
-       solution_.push_back(solution);
-       return;
+     vector<string> solution;
+     if (digits.empty()) {
+       return solution;
      }
-     for (int i = start; i < source.size(); ++i) {
-       int digit = static_cast<int>(source[i] - '0');
-       int len = strlen(digit2letter[digit]);
-       for (int j = 0; j < len; ++j) {
-         solution.push_back(digit2letter[digit][j]);
-         backtrack(i + 1, source, solution);
-         solution.pop_back();
-       }
-     }
+
+     string comb;
+     backtrack(keyboard, 0, digits, comb, solution);
+     return solution;
    }
 
  private:
-   char *digit2letter[10];
-   vector<string> solution_;
+   void backtrack(vector<string>& keyboard, int start, const string& digits, string& comb, vector<string>& solution) {
+     if (comb.size() == digits.size()) {
+       solution.push_back(comb);
+       return;
+     }
+
+     int idx = digits[start] - '0';
+     for (size_t i = 0; i < keyboard[idx].size(); ++i) {
+       comb.push_back(keyboard[idx][i]);
+       backtrack(keyboard, start + 1, digits, comb, solution);
+       comb.pop_back();
+     }
+   }
 };
+
--- LetterCombinationsofaPhoneNumber/letter_combinations_of_a_phone_number_test.cpp
+++ LetterCombinationsofaPhoneNumber/letter_combinations_of_a_phone_number_test.cpp
@@ -1,26 +1,53 @@
+/*
+ * Author : Jeremy Zhao
+ * Email  : jqzhao@live.com
+ * Date   : 2020/06/30
+ * Update : 2021/01/05
+ *
+ * Source : https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/
+ * Problem: Letter Combinations of a Phone Number
+ *
+ */
 #include <gtest/gtest.h>
-#include "letter_combinations_of_a_phone_number_ii.h"
+#include "letter_combinations_of_a_phone_number.h"
 
 using std::set;
 
 class SolutionTest : public ::testing::Test {
-    protected:
-        virtual void SetUp() {
-        }
-        virtual void TearDown() {
-        }
+ protected:
+   virtual void SetUp() {
+   }
+   virtual void TearDown() {
+   }
 
-        Solution solution_;
+   Solution solution_;
 };
 
-TEST_F(SolutionTest, case2) {
-	vector<string> vec;
-	vec = solution_.letterCombinations(string("23"));
-	EXPECT_EQ((int)vec.size(), 9);
+TEST_F(SolutionTest, case1) {
+  vector<string> vec;
+  vec = solution_.letterCombinations(string("23"));
+  EXPECT_EQ((int)vec.size(), 9);
 
-	char *chars[] = {"ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"};
-	for (size_t i = 0; i < sizeof(chars)/sizeof(char *); i++) {
-		EXPECT_TRUE(find(vec.begin(), vec.end(), string(chars[i])) != vec.end());
-	}
+  char *chars[] = {"ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"};
+  for (size_t i = 0; i < sizeof(chars)/sizeof(char *); i++) {
+    EXPECT_TRUE(find(vec.begin(), vec.end(), string(chars[i])) != vec.end());
+  }
+}
+
+TEST_F(SolutionTest, case2) {
+  vector<string> vec;
+  vec = solution_.letterCombinations(string(""));
+  EXPECT_EQ((int)vec.size(), 0);
+}
+
+TEST_F(SolutionTest, case3) {
+  vector<string> vec;
+  vec = solution_.letterCombinations(string("2"));
+  EXPECT_EQ((int)vec.size(), 3);
+
+  char *chars[] = {"a", "b", "c"};
+  for (size_t i = 0; i < sizeof(chars)/sizeof(char *); i++) {
+    EXPECT_TRUE(find(vec.begin(), vec.end(), string(chars[i])) != vec.end());
+  }
 }
 
