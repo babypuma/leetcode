200471e31e685d72aad9ae49ad336ee8e7d9a671
Jeremy Zhao <jqzhao@live.com>
2021-01-08 16:00:25 +0800

    updates Combinations

--- Combinations/combinations.h
+++ Combinations/combinations.h
@@ -2,54 +2,34 @@
  * Author : Jeremy Zhao
  * Email  : jqzhao@live.com
  * Date   : 2014/12/23
+ * Update : 2021/01/08
  *
- * Source :	https://oj.leetcode.com/problems/combinations/
+ * Source :	https://leetcode-cn.com/problems/combinations/
  * Problem:	Combinations
- * Description: 
- *	Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
- *	For example,
- *	If n = 4 and k = 2, a solution is:
- * 	[
- *  	[2,4],
- *    	[3,4],
- *	   	[2,3],
- *	    [1,2],
- *		[1,3],
- *		[1,4],
- * 	]
  *
  */
 #include <vector>
 using std::vector;
 
 class Solution {
-	public:
-	    vector<vector<int> > combine(int n, int k) { 
-			vector<int> vec(k, 0);
-			for (int i = 1; i <= n - k + 1; i++) {
-				vec[0] = i;
-				backtrack(vec, n, 1);
-			}
+ public:
+   vector<vector<int> > combine(int n, int k) { 
+     vector<vector<int> > solution;
+     vector<int> candidate;
+     backtrack(n, k, 1, candidate, solution);
+     return solution;
+   }
 
-			return vec2d;
-		}
-
-		void backtrack(vector<int> &vec, size_t n, size_t depth) {
-			if (depth >= vec.size()) {
-				vec2d.push_back(vec);
-				return;
-			}
-
-			if (n - vec[depth - 1] < vec.size() - depth) {
-				return;
-			}
-			
-			for (size_t i = vec[depth - 1] + 1; i <= n; i++) {
-				vec[depth] = i;
-				backtrack(vec, n, depth + 1);
-			}
-		}
-
-	private:
-		vector<vector<int> > vec2d;
+ private:
+   void backtrack(int n, int k, int start, vector<int>& candidate, vector<vector<int> >& solution) {
+     if (candidate.size() == k) {
+       solution.push_back(candidate);
+       return;
+     }
+     for (int i = start; i <= n; ++i) {
+       candidate.push_back(i);
+       backtrack(n, k, i + 1, candidate, solution);
+       candidate.pop_back();
+     }
+   }
 };
--- Combinations/combinations_test.cpp
+++ Combinations/combinations_test.cpp
@@ -11,44 +11,44 @@
 #include "combinations.h"
 
 class SolutionTest : public ::testing::Test {
-    protected:
-        virtual void SetUp() {
-        }
-        virtual void TearDown() {
-        }
+ protected:
+   virtual void SetUp() {
+   }
+   virtual void TearDown() {
+   }
 
-        Solution solution_;
+   Solution solution_;
 };
 
 TEST_F(SolutionTest, case1) {
-	vector<vector<int> > vec2d = solution_.combine(10, 1);
-	EXPECT_EQ((int)vec2d.size(), 10);
-	for (size_t i = 0; i < vec2d.size(); i++) {
-		EXPECT_EQ((int)vec2d[i].size(), 1);
-	}
+  vector<vector<int> > vec2d = solution_.combine(10, 1);
+  EXPECT_EQ((int)vec2d.size(), 10);
+  for (size_t i = 0; i < vec2d.size(); i++) {
+    EXPECT_EQ((int)vec2d[i].size(), 1);
+  }
 }
 
 TEST_F(SolutionTest, case2) {
-	vector<vector<int> > vec2d = solution_.combine(10, 2);
-	EXPECT_EQ((int)vec2d.size(), 45);
-	for (size_t i = 0; i < vec2d.size(); i++) {
-		EXPECT_EQ((int)vec2d[i].size(), 2);
-	}
+  vector<vector<int> > vec2d = solution_.combine(10, 2);
+  EXPECT_EQ((int)vec2d.size(), 45);
+  for (size_t i = 0; i < vec2d.size(); i++) {
+    EXPECT_EQ((int)vec2d[i].size(), 2);
+  }
 }
 
 TEST_F(SolutionTest, case3) {
-	vector<vector<int> > vec2d = solution_.combine(10, 5);
-	EXPECT_EQ((int)vec2d.size(), 252);
-	for (size_t i = 0; i < vec2d.size(); i++) {
-		EXPECT_EQ((int)vec2d[i].size(), 5);
-	}
+  vector<vector<int> > vec2d = solution_.combine(10, 5);
+  EXPECT_EQ((int)vec2d.size(), 252);
+  for (size_t i = 0; i < vec2d.size(); i++) {
+    EXPECT_EQ((int)vec2d[i].size(), 5);
+  }
 }
 
 TEST_F(SolutionTest, case4) {
-	vector<vector<int> > vec2d = solution_.combine(1, 1);
-	EXPECT_EQ((int)vec2d.size(), 1);
-	for (size_t i = 0; i < vec2d.size(); i++) {
-		EXPECT_EQ((int)vec2d[i].size(), 1);
-	}
+  vector<vector<int> > vec2d = solution_.combine(1, 1);
+  EXPECT_EQ((int)vec2d.size(), 1);
+  for (size_t i = 0; i < vec2d.size(); i++) {
+    EXPECT_EQ((int)vec2d[i].size(), 1);
+  }
 }
 
