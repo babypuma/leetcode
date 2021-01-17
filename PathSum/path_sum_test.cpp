3e76fc0f01e849e55ed25022d1b0d031145bee51
Jeremy Zhao <jqzhao@live.com>
2021-01-17 23:29:36 +0800

    adds PathSum

--- PathSum/path_sum_test.cpp
+++ PathSum/path_sum_test.cpp
@@ -2,8 +2,9 @@
  * Author : Jeremy Zhao
  * Email  : jqzhao@live.com
  * Date   : 2014/11/14
+ * Update : 2021/01/17
  *
- * Source : https://oj.leetcode.com/problems/path-sum/
+ * Source : https://leetcode-cn.com/problems/path-sum/
  * Problem:	Path Sum
  *
  */
@@ -11,26 +12,33 @@
 #include "path_sum.h"
 
 class SolutionTest : public ::testing::Test {
-    protected:
-        virtual void SetUp() {
-        }
-        virtual void TearDown() {
-        }
+ protected:
+   virtual void SetUp() {
+   }
+   virtual void TearDown() {
+   }
 
-        Solution solution_;
+   Solution solution_;
 };
 
 TEST_F(SolutionTest, case1) {
-	TreeNode *root = new TreeNode(5);
-	root->left = new TreeNode(4);
-	root->left->left = new TreeNode(11);
-	root->left->left->left = new TreeNode(7);
-	root->left->left->right = new TreeNode(2);
-	root->right = new TreeNode(8);
-	root->right->left = new TreeNode(13);
-	root->right->right = new TreeNode(4);
-	root->right->right->right = new TreeNode(1);
+  TreeNode *root = new TreeNode(5);
+  root->left = new TreeNode(4);
+  root->left->left = new TreeNode(11);
+  root->left->left->left = new TreeNode(7);
+  root->left->left->right = new TreeNode(2);
+  root->right = new TreeNode(8);
+  root->right->left = new TreeNode(13);
+  root->right->right = new TreeNode(4);
+  root->right->right->right = new TreeNode(1);
 
-	EXPECT_TRUE(solution_.hasPathSum(root, 22));
+  EXPECT_TRUE(solution_.hasPathSum(root, 22));
+}
+
+TEST_F(SolutionTest, case2) {
+  TreeNode *root = new TreeNode(1);
+  root->right = new TreeNode(2);
+
+  EXPECT_FALSE(solution_.hasPathSum(root, 1));
 }
 
