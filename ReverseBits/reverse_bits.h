17b39635804398f51e83c2df07bf0147bf710bee
Jeremy Zhao <jqzhao@live.com>
2021-01-19 23:57:48 +0800

    updates ReverseBits

--- ReverseBits/reverse_bits.h
+++ ReverseBits/reverse_bits.h
@@ -2,39 +2,32 @@
  * Author : Jeremy Zhao
  * Email  : jqzhao@live.com
  * Date   : 2015/06/16
+ * Update : 2021/01/19
  *
- * Source : https://leetcode.com/problems/reverse-bits/
- * Problem:	Reverse Bits
- * Description: 
- *	Reverse bits of a given 32 bits unsigned integer.
- *
- * 	For example, given input 43261596 (represented in binary as 00000010100101000001111010011100), 
- *	return 964176192 (represented in binary as 00111001011110000010100101000000).
- *
- * 	Follow up:
- * 	If this function is called many times, how would you optimize it?
+ * Source : https://leetcode-cn.com/problems/reverse-bits/
+ * Problem: Reverse Bits
  *
  */
 class Solution {
-	public: 
-		uint32_t reverseBits(uint32_t n) {
-			int mark = 0x1;
-			int bits = 31;
-			uint32_t val = 0;
-			int i;
-			for (i = 0; i < 16; i++) {
-				val |= (n & mark) << bits;
-				mark <<= 1;
-				bits -= 2;
-			}
+ public: 
+   uint32_t reverseBits(uint32_t n) {
+     uint32_t mark = 0x1;
+     int bits = 31;
+     uint32_t val = 0;
+     int i;
+     for (i = 0; i < 16; i++) {
+       val |= (n & mark) << bits;
+       mark <<= 1;
+       bits -= 2;
+     }
 
-			bits = 1;
-			for (i = 16; i < 32; i++) {
-				val |= (n & mark) >> bits;
-				mark <<= 1;
-				bits += 2;
-			}
+     bits = 1;
+     for (i = 16; i < 32; i++) {
+       val |= (n & mark) >> bits;
+       mark <<= 1;
+       bits += 2;
+     }
 
-			return val;
-		}
+     return val;
+   }
 };
--- ReverseBits/reverse_bits_test.cpp
+++ ReverseBits/reverse_bits_test.cpp
@@ -2,25 +2,26 @@
  * Author : Jeremy Zhao
  * Email  : jqzhao@live.com
  * Date   : 2015/06/16
+ * Update : 2021/01/19
  *
- * Source : https://leetcode.com/problems/reverse-bits/
- * Problem:	Reverse Bits
+ * Source : https://leetcode-cn.com/problems/reverse-bits/
+ * Problem: Reverse Bits
  *
  */
 #include <gtest/gtest.h>
 #include "reverse_bits.h"
 
 class SolutionTest : public ::testing::Test {
-    protected:
-        virtual void SetUp() {
-        }
-        virtual void TearDown() {
-        }
+ protected:
+   virtual void SetUp() {
+   }
+   virtual void TearDown() {
+   }
 
-        Solution solution_;
+   Solution solution_;
 };
 
 TEST_F(SolutionTest, case1) {
-	EXPECT_EQ(solution_.reverseBits(43261596), 964176192);
+  EXPECT_EQ(solution_.reverseBits(43261596), 964176192);
 }
 
