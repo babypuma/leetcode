8795d8fe2b95c682d3e2a4867b35dadc8ad5ad69
Jeremy Zhao <jqzhao@live.com>
2021-03-03 16:17:08 +0800

    updates BinaryTreeInorderTraversal

--- BinaryTreeInorderTraversal/binary_tree_inorder_traversal.h
+++ BinaryTreeInorderTraversal/binary_tree_inorder_traversal.h
@@ -2,45 +2,43 @@
  * Author : Jeremy Zhao
  * Email  : jqzhao@live.com
  * Date   : 2014/11/01
+ * Update : 2021/03/03
  *
  * Source : https://oj.leetcode.com/problems/binary-tree-inorder-traversal/
  * Problem:	Binary Tree Inorder Traversal
- * Description: 
- *	Given a binary tree, return the inorder traversal of its nodes' values.
  *
  */
-#include <stdio.h>
 #include <stack>
 #include <vector>
 using std::stack;
 using std::vector;
 
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 class Solution {
-	public:
-		vector<int> inorderTraversal(TreeNode *root) {
-			vector<int> vec;
-			stack<TreeNode *> st;
-			while (root != NULL || !st.empty()) {
-				while (root != NULL) {
-					st.push(root);
-					root = root->left;
-				}
+ public:
+   vector<int> inorderTraversal(TreeNode *root) {
+     vector<int> vec;
+     stack<TreeNode *> st;
+     while (root != nullptr || !st.empty()) {
+       if (root != nullptr) {
+         st.push(root);
+         root = root->left;
+       } else {
+         root = st.top();
+         st.pop();
+         vec.push_back(root->val);
+         root = root->right;
+       }
+     }
 
-				if (root == NULL) {
-					root = st.top();
-					st.pop();
-					vec.push_back(root->val);
-					root = root->right;
-				}
-			}
-
-			return vec;
-		}
+     return vec;
+   }
 };
